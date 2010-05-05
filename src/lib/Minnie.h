289bdaafae21ab0e733c039669caf201f0de4ea0
wolf <wolf@cs.cmu.edu>
2010-05-05 02:26:51 -0400

    Updated, more fixes.

--- src/lib/Minnie.h
+++ src/lib/Minnie.h
@@ -10,11 +10,11 @@
 
 using namespace std;
 
-class MapReduceSpecification { 
+class MapReduceSpecification {
   public:
         MapReduceSpecification() {};
 	~MapReduceSpecification();
-	vector<string*> getInputFiles() { return input; };
+	vector<string> getInputFiles() { return input; };
 	string getOutputPath() { return output; };
 	string getDfsMaster() {return dfs_master; };
 	string getSoName() { return so_name; };
@@ -22,7 +22,7 @@ class MapReduceSpecification {
 	JobID getMaxJobsPerNode() { return maxJobs; };
 	JobID getMaxMaps() { return maxMaps; };
 	JobID getMaxReduces() { return maxReduces; };
-	
+
 	void setOutputPath(string o) { output = o;}
 	void setDfsMaster(string master) {dfs_master = master; };
 	void setSoName(string sname) { so_name = sname; };
@@ -30,20 +30,18 @@ class MapReduceSpecification {
 	void setMaxJobsPerNode(JobID maxj) { maxJobs = maxj;};
 	void setMaxMaps(JobID maxm) { maxMaps = maxm;};
 	void setMaxReduces(JobID maxr) {maxReduces = maxr;};
-	void addInput(string* in) {input.push_back(in);};
+	void addInput(string in) {input.push_back(in);};
 
   private:
-	vector<string*> input;
+	vector<string> input;
 	string output;
         string dfs_master;
 	string so_name;
 	uint16_t dfs_port;
 	JobID maxJobs;
 	JobID maxMaps;
-	JobID maxReduces; 
-   
+	JobID maxReduces;
 };
-
 
 class MapReduceResult
 {
@@ -57,9 +55,4 @@ class MapReduceResult
                 JobID completedReduces;
                 JobID numberNodes;
 };
-
-
-
-
-
 #endif //Minnie_H_
