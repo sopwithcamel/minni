d4c430fdc6cb09b7544f2b8c11edadd79bd3c2e8
Wolfgang Richter <wolf@cs.cmu.edu>
2010-06-16 13:41:53 -0400

    Autocreate necessary folders when running autotools.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+
+if [ ! -d m4 ]
+then
+	mkdir m4
+fi
+
+if [ ! -d build-aux ]
+then
+	mkdir build-aux
+fi
+
+if [ ! -d src/common ]
+then
+	mkdir src/common
+fi
+
 make -k maintainer-clean
 autoreconf --install --force
