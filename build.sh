ea92ba2bec0705c896dfa5622f05c4302818152e
Wolfgang Richter <wolf@cs.cmu.edu>
2010-06-25 13:39:58 -0400

    Benjamin pointed out we can default to silent rules on and not need the configure option.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LDFLAGS=-L/home/wolf/Desktop/kosmostest/kfs-0.5/build/lib CPPFLAGS=-I/home/wolf/Desktop/kosmostest/kfs-0.5/build/include/kfs/ ./configure --enable-silent-rules
+LDFLAGS=-L/home/wolf/Desktop/kosmostest/kfs-0.5/build/lib CPPFLAGS=-I/home/wolf/Desktop/kosmostest/kfs-0.5/build/include/kfs/ ./configure
 
 make
