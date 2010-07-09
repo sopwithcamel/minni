5358c390cf251152979c0cf6f0edfed1044474fe
hamur <hamur@fawn0.irp-cluster.research.intel-research.net>
2010-07-09 02:13:39 -0400

    Changes to ExtendableHashtable allow this to be used in different stages of the Map-side process. The first option does partial aggregation and writes out in a format amenable to nsort. The second option allows the output of nsort to be totally aggregated. Includes bugfixes for not clearing hashtable after dumping for finalize() and others.

--- src/lib/ExtendableHashtable.h
+++ src/lib/ExtendableHashtable.h
@@ -14,6 +14,9 @@ using namespace std;
 #include "PartialAgg.h"
 #define GetCurrentDir getcwd
 
+#define REGULAR_SERIALIZE	0
+#define NSORT_SERIALIZE		1
+
 typedef std::tr1::unordered_map<string, PartialAgg*> Hash;
 
 class ExtendableHashtable {
@@ -25,9 +28,11 @@ class ExtendableHashtable {
 		bool add(const string &key, const string &value); /* add to existing PAO */
 		bool finalize(string fname); /* true on success, false otherwise */
 		bool clear();
+		void setSerializeFormat(int);
 
 	private:
 		Hash hashtable;
+		bool regularSerialize; /* Regular serialize? */
 		uint64_t partid;
 		uint64_t capacity; /* maximum capacity of map before dumping */
 		uint64_t dumpNumber; /* monotonically increasing dump sequence number */
@@ -36,6 +41,7 @@ class ExtendableHashtable {
 		bool insert(string key, PartialAgg* pao); /* returns PAO from map, or pao if not in map already */
 		bool dumpHashtable(string fname); /* true if dump succeeds, false otherwise */
 		string getDumpFileName(uint64_t);
+		void serialize(FILE*, string, string);
 		void serialize(FILE*, char, uint64_t, const char*, uint64_t, const char*);
 		int deSerialize(FILE*, char*, uint64_t*, char**, uint64_t*, char**);
 
