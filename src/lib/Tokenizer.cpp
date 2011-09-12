a83be229ead664fbca8b55bc9eb3c523541995ee
Hrishikesh Amur <amur@gatech.edu>
2011-09-12 01:23:31 -0400

    Remove realloc circus from Tokenizer

--- src/lib/Tokenizer.cpp
+++ src/lib/Tokenizer.cpp
@@ -1,5 +1,4 @@
 #include "Tokenizer.h"
-#include "Util.h"
 
 Tokenizer::Tokenizer(Aggregator* agg, PartialAgg* emptyPAO, 
 			PartialAgg* (*createPAOFunc)(const char* t)) :
@@ -27,13 +26,11 @@ void* Tokenizer::operator()(void* buffer)
 	char* tok_buf = (char*) buffer;	 
 	int tok_ctr = 0;
 	size_t this_list_ctr = 0;
-	size_t this_list_size = 1;
 
 	uint64_t num_buffers = aggregator->getNumBuffers();
 
-	/* TODO: don't the next two statements need to executed atomically? */
-	pao_list[next_buffer] = (PartialAgg**)malloc(sizeof(PartialAgg*));
-	PartialAgg*** this_pao_list = &pao_list[next_buffer];
+	pao_list[next_buffer] = (PartialAgg**)malloc(sizeof(PartialAgg*) * MAX_KEYS_PER_TOKEN);
+	PartialAgg** this_pao_list = pao_list[next_buffer];
 	next_buffer = (next_buffer + 1) % num_buffers; 
 	
 	spl = strtok(tok_buf, " .\n\r\'\"?,;:!*()-\uFEFF");
@@ -44,30 +41,16 @@ void* Tokenizer::operator()(void* buffer)
 	while (1) {
 	PartialAgg* new_pao = createPAO(spl); 
 //		fprintf(stderr, "tok: %d", tok_ctr++);
-		if (this_list_ctr >= this_list_size) {
-			this_list_size += LIST_SIZE_INCR;
-			if (call_realloc(this_pao_list, this_list_size) == NULL) {
-				perror("realloc failed");
-				return NULL;
-			}
-			assert(this_list_ctr < this_list_size);
-		}
-		(*this_pao_list)[this_list_ctr++] = new_pao;
+
+		this_pao_list[this_list_ctr++] = new_pao;
+		assert(this_list_ctr < MAX_KEYS_PER_TOKEN);
 
 		spl = strtok(NULL, " .\n\r\'\"?,;:!*()-\uFEFF");
 		if (spl == NULL) {
 			break;
 		}
 	}
-	if (this_list_ctr >= this_list_size) {
-		this_list_size += LIST_SIZE_INCR;
-		if (call_realloc(this_pao_list, this_list_size) == NULL) {
-			perror("realloc failed");
-			return NULL;
-		}
-		assert(this_list_ctr < this_list_size);
-	}
-	(*this_pao_list)[this_list_ctr++] = emptyPAO;
-	return *this_pao_list;
+	this_pao_list[this_list_ctr++] = emptyPAO;
+	return this_pao_list;
 }
 
