8223d32a87aa4a07753a19b6f3fc080e522137ef
Hrishikesh Amur <amur@gatech.edu>
2011-10-10 10:58:54 -0400

    Store compiles now

--- src/lib/Store.cpp
+++ src/lib/Store.cpp
@@ -9,46 +9,75 @@ Store::Store(Aggregator* agg,
 		next_buffer(0),
 		tokens_processed(0)
 {
-	hasher = new StoreHasher();
-	agger = new StoreAggregator();
-	writer = new StoreWriter();
+	hasher = new StoreHasher(this);
+	agger = new StoreAggregator(this);
+	writer = new StoreWriter(this);
 }
 
-StoreHasher::StoreHasher() :
+Store::~Store()
+{
+}
+
+bool Store::key_present(size_t bin_offset, size_t bin_index)
+{
+	// TODO: Implement!
+	return false;
+}
+
+void Store::set_key_present(size_t bin_offset, size_t bin_index)
+{
+	// TODO: Implement
+}
+
+
+StoreHasher::StoreHasher(Store* store) :
 		filter(/*serial=*/true),
+		store(store),
 		next_buffer(0),
 		tokens_processed(0)
 {
-	uint64_t num_buffers = aggregator->getNumBuffers();
+	uint64_t num_buffers = store->aggregator->getNumBuffers();
 	offset_list = (size_t**)malloc(sizeof(size_t*) * num_buffers);
 	value_list = (char***)malloc(sizeof(char**) * num_buffers);
 	send = (FilterInfo**)malloc(sizeof(FilterInfo*) * num_buffers);
 	// Allocate buffers and structure to send results to next filter
 	for (int i=0; i<num_buffers; i++) {
 		offset_list[i] = (size_t*)malloc(sizeof(size_t) 
-			* max_keys_per_token);
+			* store->max_keys_per_token);
 		value_list[i] = (char**)malloc(sizeof(char*)
-			* max_keys_per_token);
+			* store->max_keys_per_token);
+		for (int j=0; j<store->max_keys_per_token; j++) {
+			value_list[i][j] = (char*)malloc(VALUE_SIZE);
+		}
 		send[i] = (FilterInfo*)malloc(sizeof(FilterInfo));
 	}
 }
 
 StoreHasher::~StoreHasher()
 {
-	uint64_t num_buffers = aggregator->getNumBuffers();
+	uint64_t num_buffers = store->aggregator->getNumBuffers();
 	for (int i=0; i<num_buffers; i++) {
 		free(offset_list[i]);
+		for (int j=0; j<store->max_keys_per_token; j++) {
+			free(value_list[i][j]);
+		}
 		free(value_list[i]);
 		free(send[i]);
 	}
-	free(offset_list)
+	free(offset_list);
 	free(value_list);
 	free(send);
 }
 
+uint64_t StoreHasher::findBinOffset(char* key)
+{
+	// TODO: Implement
+	return 0;
+}
+
 void* StoreHasher::operator()(void* pao_list)
 {
-	char *key, *value;
+	char *key, *value, *offset;
 	uint64_t ind = 0;
 	PartialAgg* pao;
 
@@ -59,7 +88,7 @@ void* StoreHasher::operator()(void* pao_list)
 	size_t* this_offset_list = offset_list[next_buffer];
 	char** this_value_list = value_list[next_buffer];
 	FilterInfo* this_send = send[next_buffer];
-	next_buffer = (next_buffer + 1) % aggregator->getNumBuffers();
+	next_buffer = (next_buffer + 1) % store->aggregator->getNumBuffers();
 
 	uint64_t num_paos_in_bin = BINSIZE / PAOSIZE;
 	char* buf = (char*)malloc(BINSIZE);
@@ -68,11 +97,11 @@ void* StoreHasher::operator()(void* pao_list)
 		pao = pao_l[ind];
 
 		// Find bin using hash function
-		size_t bin_offset = findBinOffset(pao->key);
-		assert(bin_offset <= store_size - BINSIZE);
+		uint64_t bin_offset = findBinOffset(pao->key);
+		assert(bin_offset <= store->store_size - BINSIZE);
 
 		// Read value
-		ssize_t n_read = pread64(fd, buf, BINSIZE, bin_offset);
+		ssize_t n_read = pread64(store->store_fd, buf, BINSIZE, bin_offset);
 		
 		// Find offset of value in external HT
 		// TODO: Change this!
@@ -81,22 +110,21 @@ void* StoreHasher::operator()(void* pao_list)
 			sum += pao->key[i] << i;
 		}
 		size_t bin_ind = sum % num_paos_in_bin;
-		if (!key_present(bin_offset, bin_ind)) { // key not present
+		if (!store->key_present(bin_offset, bin_ind)) { // key not present
 			goto key_not_present;
 		}
 
 		// Key is present; check if it's what we want
-		char* offset = buf + bin_ind * PAOSIZE;
+		offset = buf + bin_ind * PAOSIZE;
 		while (memcmp(pao->key, offset, strlen(pao->key))) {
 			// Not the key we want; move to next slot
 			bin_ind = (bin_ind + 1) % num_paos_in_bin;
 			// If slot is empty, our key is not present
-			if (!key_present(bin_offset, bin_ind))
+			if (!store->key_present(bin_offset, bin_ind))
 				goto key_not_present;
 		}
 		// Key is present, and matches our key; copy
-		this_value_list[ind] = (char*)malloc(PAOSIZE);
-		memcpy(this_value_list[ind], offset, PAOSIZE);
+		memcpy(this_value_list[ind], offset, VALUE_SIZE);
 		this_offset_list[ind] = bin_offset + bin_ind * PAOSIZE;
 		
 		continue;
@@ -105,36 +133,37 @@ key_not_present:
 		// Also set slot as occupied.
 		this_value_list[ind] = NULL;
 		this_offset_list[ind] = bin_offset + bin_ind * PAOSIZE;
-		set_key_present(bin_offset, bin_ind);
+		store->set_key_present(bin_offset, bin_ind);
 		
 	}
 
 	this_send->result = pao_l;
 	this_send->length = recv_length;
-	this_send->result1 = this_merge_list;
-	this_send->result3 = merge_list_ctr;
+	this_send->result1 = this_value_list;
+	this_send->result2 = this_offset_list;
 	return this_send;
 }
 
-StoreAggregator::StoreAggregator() :
+StoreAggregator::StoreAggregator(Store* store) :
 		filter(/*serial=*/true),
+		store(store),
 		next_buffer(0),
 		tokens_processed(0)
 {
-	uint64_t num_buffers = aggregator->getNumBuffers();
+	uint64_t num_buffers = store->aggregator->getNumBuffers();
 	value_list = (char***)malloc(sizeof(char**) * num_buffers);
 	send = (FilterInfo**)malloc(sizeof(FilterInfo*) * num_buffers);
 	// Allocate buffers and structure to send results to next filter
 	for (int i=0; i<num_buffers; i++) {
 		value_list[i] = (char**)malloc(sizeof(char*)
-			* max_keys_per_token);
+			* store->max_keys_per_token);
 		send[i] = (FilterInfo*)malloc(sizeof(FilterInfo));
 	}
 }
 
 StoreAggregator::~StoreAggregator()
 {
-	uint64_t num_buffers = aggregator->getNumBuffers();
+	uint64_t num_buffers = store->aggregator->getNumBuffers();
 	for (int i=0; i<num_buffers; i++) {
 		free(value_list[i]);
 		free(send[i]);
@@ -152,28 +181,29 @@ void* StoreAggregator::operator()(void* pao_list)
 	FilterInfo* recv = (FilterInfo*)pao_list;
 	PartialAgg** pao_l = (PartialAgg**)recv->result;
 	uint64_t recv_length = (uint64_t)recv->length;	
-	size_t* recv_off_list = (size_t*)recv->result1;
-	char** recv_val_list = (char**)recv->result3;
+	char** recv_val_list = (char**)recv->result1;
+	size_t* recv_off_list = (size_t*)recv->result2;
 
 	char** this_value_list = value_list[next_buffer];
 	FilterInfo* this_send = send[next_buffer];
-	next_buffer = (next_buffer + 1) % aggregator->getNumBuffers();
+	next_buffer = (next_buffer + 1) % store->aggregator->getNumBuffers();
 
-	while (ind < recv_length) {
+	for (ind=0; ind < recv_length; ind++) {
 		pao = pao_l[ind];
 		// Aggregate values
-		// destroy PAO	
+		pao->add(recv_val_list[ind] + MAX_KEYSIZE);
 		ind++;
 	}
 
-	this_send->result = this_value_list;
+	this_send->result = pao_l;
 	this_send->length = recv_length;
 	this_send->result1 = recv_off_list;
 	return this_send;
 }
 
-StoreWriter::StoreWriter() :
+StoreWriter::StoreWriter(Store* store) :
 		filter(/*serial=*/true),
+		store(store),
 		next_buffer(0),
 		tokens_processed(0)
 {
@@ -191,10 +221,12 @@ void* StoreWriter::operator()(void* pao_list)
 	FilterInfo* recv = (FilterInfo*)pao_list;
 	PartialAgg** pao_l = (PartialAgg**)recv->result;
 	uint64_t recv_length = (uint64_t)recv->length;	
+	size_t* recv_off_list = (size_t*)recv->result1;
 
 	while (ind < recv_length) {
 		pao = pao_l[ind];
 		// Write value at offset
+		pwrite64(store->store_fd, pao->key, PAOSIZE, recv_off_list[ind]);
 		ind++;
 	}
 }
--- src/lib/Store.h
+++ src/lib/Store.h
@@ -19,23 +19,36 @@
 #define	BINS		100000000
 #define	BINSIZE		4096
 #define PAOSIZE		32
+#define MAX_KEYSIZE	12
+
+class StoreHasher;
+class StoreAggregator;
+class StoreWriter;
 
 class Store {
+	friend class StoreHasher;
+	friend class StoreAggregator;
+	friend class StoreWriter;
 public:
 	const size_t max_keys_per_token;
 
-	Store(Aggregator* agg);
+	Store(Aggregator* agg,
+			void (*destroyPAOFunc)(PartialAgg* p),
+			const size_t max_keys);
 	~Store();
 	void (*destroyPAO)(PartialAgg* p);
 private:
 	int store_fd;
+	size_t store_size;
+	int next_buffer;
+	uint64_t tokens_processed;
 	Aggregator* aggregator;
 	/* Stages of the pipeline */
 	StoreHasher* hasher;
 	StoreAggregator* agger;
 	StoreWriter* writer;
 
-	bool key_not_present(size_t bin_offset, size_t bin_index);
+	bool key_present(size_t bin_offset, size_t bin_index);
 	void set_key_present(size_t bin_offset, size_t bin_index);
 };
 
@@ -43,11 +56,12 @@ private:
  * Consumes: list of PAOs to be externally aggregated
  * Produces: lists of matching offsets and values
  */
-class StoreHasher : public Store, public tbb::filter {
+class StoreHasher : public tbb::filter {
 public:
-	StoreHasher();
+	StoreHasher(Store* store);
 	~StoreHasher();
 private:
+	Store* store;
 	uint64_t tokens_processed;
 
 	/* Pointer to lists holding values to be moved on */
@@ -57,17 +71,19 @@ private:
 	size_t next_buffer;
 
 	void* operator()(void* pao_list);
-}
+	uint64_t findBinOffset(char* key);
+};
 
 /**
  * Consumes: lists of PAOs, offsets and values
  * Produces: lists of merged values and offsets
  */
-class StoreAggregator : public Store, public tbb::filter {
+class StoreAggregator : public tbb::filter {
 public:
-	StoreAggregator();
+	StoreAggregator(Store* store);
 	~StoreAggregator();
 private:
+	Store* store;
 	uint64_t tokens_processed;
 
 	/* Pointer to lists holding values to be moved on */
@@ -76,21 +92,22 @@ private:
 	size_t next_buffer;
 
 	void* operator()(void* pao_list);
-}
+};
 
 /**
  * Consumes: lists of merged values and offsets
  * Produces: -
  */
-class StoreWriter : public Store, public tbb::filter {
+class StoreWriter : public tbb::filter {
 public:
-	StoreWriter();
+	StoreWriter(Store* store);
 	~StoreWriter();
 private:
+	Store* store;
 	uint64_t tokens_processed;
 	size_t next_buffer;
 	FilterInfo** send;
 	void* operator()(void* pao_list);
-}
+};
 
 #endif // LIB_STORE_H
