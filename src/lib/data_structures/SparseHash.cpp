75368467bea325728dfad5a374a8d9e17e02b493
Hrishikesh Amur <amur@gatech.edu>
2012-01-24 18:01:33 -0500

    Correct BF'ed code

--- src/lib/data_structures/SparseHash.cpp
+++ src/lib/data_structures/SparseHash.cpp
@@ -16,7 +16,7 @@ bool SparseHash::insert(void* key, PartialAgg* value, PartialAgg**& evicted,
         size_t& num_evicted)
 {
     bool ret;
-    std::string k = std::string((char*)key);
+    char* k = (char*)key;
     PartialAgg* mg = NULL;
     mg = accumulator_[k];
     if (mg) {
@@ -25,22 +25,22 @@ bool SparseHash::insert(void* key, PartialAgg* value, PartialAgg**& evicted,
     } else {
         accumulator_[k] = value;
         ret = true;
+        if (++numElements_ >= capacity_) {
+            size_t evictCtr = 0;
+            for (Hash::iterator it=accumulator_.begin(); it != accumulator_.end(); 
+                    it++) {
+                evicted[evictCtr++] = it->second;
+                accumulator_.erase(it);
+                if (evictCtr == evictAtTime_)
+                    break;
+            }
+            numElements_ -= evictCtr;
+            num_evicted = evictCtr;
+        } else
+            num_evicted = 0;
+        // insert invalidates read iterator
+        readIterator_ = accumulator_.end();
     }
-    if (++numElements_ >= capacity_) {
-        size_t evictCtr = 0;
-        for (Hash::iterator it=accumulator_.begin(); it != accumulator_.end(); 
-                it++) {
-            evicted[evictCtr++] = it->second;
-            accumulator_.erase(it);
-            if (evictCtr == evictAtTime_)
-                break;
-        }
-        numElements_ -= evictCtr;
-        num_evicted = evictCtr;
-    } else
-        num_evicted = 0;
-    // insert invalidates read iterator
-    readIterator_ = accumulator_.end();
     return ret;
 }
 
@@ -49,7 +49,7 @@ bool SparseHash::nextValue(void*& key, PartialAgg*& value)
     if (readIterator_ == accumulator_.end())
         readIterator_ = accumulator_.begin();
     if (readIterator_ != accumulator_.end()) {
-        key = (void*)(readIterator_->first).c_str();
+        key = (void*)(readIterator_->first);
         value = readIterator_->second;
         //  erase keeps iterator valid
         Hash::iterator t = readIterator_++;
