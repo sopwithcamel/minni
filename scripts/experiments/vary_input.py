e9162d967bd9f324b845fa9ee144202a1a19f832
Hrishikesh Amur <amur@gatech.edu>
2012-04-12 17:46:23 -0400

    Script to run different datasets

---
+++ scripts/experiments/vary_input.py
@@ -0,0 +1,15 @@
+import os,sys,subprocess
+
+buffer_size = 30*1024**2;
+fanout = 8
+for uniq in [10, 20, 30, 40, 50, 60, 70]:
+    for rep in [int(100/uniq), int(1000/uniq)]:
+        fil = str(uniq) + "mil" + str(rep) + "x" + "10b.txt"
+        # Generate file
+        subprocess.call("../inputgen/genr " + str(uniq * 1000000) + " " + str(rep) + " 7", shell=True)
+        # Copy to kfs
+        subprocess.call("cd ~/code/kfs-0.5/build/bin/tools; ./kfsshell -s cayenne -p 20000 -q rm dataset.txt; ./cptokfs -s cayenne -p 20000 -d /mnt/hamur/randinput.txt -k ./dataset.txt; cd -", shell=True)
+        # Generate new config file
+        subprocess.call("../config/mod -minni__input_files dataset.txt -minni__internal__cbt__buffer_size " + str(buffer_size) + " -minni__internal__cbt__fanout " + str(fanout), shell=True)
+        subprocess.call("cd ../monitoring/; ./run_minni_local.sh; cd -", shell=True)
+        subprocess.call("sleep 5", shell=True)
