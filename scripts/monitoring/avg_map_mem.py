8f442f825ef637938a751c73b04cfad4728631b8
Hrishikesh Amur <amur@gatech.edu>
2012-04-13 22:09:46 -0400

    Update avg script

--- scripts/monitoring/avg_map_mem.py
+++ scripts/monitoring/avg_map_mem.py
@@ -8,10 +8,5 @@ for line in file1:
   line = line.split(",")
   mem.append(float(line[0]))
   cpu.append(float(line[1]))
-  if int(line[2]) > 0:
-    disku.append(1)
-  else:
-    disku.append(0)
 print "Map memory: ", numpy.mean(mem)
 print "Map CPU: ", numpy.mean(cpu)
-print "Map disk utilization: ", numpy.mean(disku)
