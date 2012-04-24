0acdf94177737a829d491508ff4d5e3a62b1a3a4
Wolfgang Richter <wolf@cs.cmu.edu>
2012-04-24 00:25:45 -0400

    apps: rewrote image app to use protobufs, testing ongoing

--- src/lib/apps/img.cpp
+++ src/lib/apps/img.cpp
@@ -5,7 +5,7 @@
 #define NEAREST		5
 
 /**
- * Creates a PAO that represents the relationship between a query image and a 
+ * Creates a PAO that represents the relationship between a query image and a c
  * reference image. The pHash values for both images are calculated and the
  * absolute value of the difference is used as the distance. Note that the
  * PAO doesn't store the image data.
@@ -20,16 +20,15 @@
 ImagePAO::ImagePAO(Token* token)
 {
 	if (token == NULL) {
-		key = NULL;
-		neigh_list = NULL;
+		pb.set_key(NULL);
+        /* nada; leave at size 0 pb.add_neighbors(); */
 		return;
 	}
 
 	// Set key equal to the query file name
-	key = (char*)malloc(token->token_sizes[2]);
-	strcpy(key, (char*)(token->tokens[2]));
+	pb.set_key((char*) (token->tokens[2]));
 
-//	fprintf(stderr, "Key: %s\t", key);
+    //	fprintf(stderr, "Key: %s\t", key);
 	// Calculate image hash
 	CImg<unsigned char> img;
 	img.load_jpeg_buffer((JOCTET*)(token->tokens[3]), token->token_sizes[3]);
@@ -37,25 +36,20 @@ ImagePAO::ImagePAO(Token* token)
 
 	uint64_t ne_hash;
 	fprintf(stderr, "Neigh: %s\n", (char*)token->tokens[0]);
-	Neighbor* n = new Neighbor((char*)(token->tokens[0]),
-			token->token_sizes[0]);
+    imagepao::Neighbor* n = pb.add_neighbors();
+	n->set_key((char*)(token->tokens[0]));
+
 	CImg<unsigned char> ne_img;
 	ne_img.load_jpeg_buffer((JOCTET*)(token->tokens[1]),
 			token->token_sizes[1]);
 	pHash(ne_img, ne_hash);
-	n->distance = abs((long)(hash - ne_hash));
+	n->set_distance(abs((long)(hash - ne_hash)));
 
-	neigh_list = new std::list<Neighbor>();
-	neigh_list->push_back(*n);
 //	fprintf(stderr, "Found neighbor: %s, %ld\n", n->key, n->distance);
 }
 
 ImagePAO::~ImagePAO()
 {
-	if (key)
-		free(key);
-	if (neigh_list)
-		neigh_list->clear();
 }
 
 size_t ImagePAO::create(Token* t, PartialAgg** p)
@@ -73,112 +67,46 @@ size_t ImagePAO::create(Token* t, PartialAgg** p)
  * Arguments:
  *  - val is expected to be a mutable array
  */ 
-void ImagePAO::add(void* val)
-{	
-	char* spl;
-	char* read_buf = (char*)val;
-	Neighbor* n;
-	std::list<Neighbor>::iterator it; 
-	// read key
-	spl = strtok(read_buf, " ");
-	while (true) {
-		/* read in neighbor key */
-		spl = strtok(NULL, " ");
-		if (spl == NULL)
-			break;
-		/* create new neighbor */
-		n = new Neighbor(spl, strlen(spl)+1);
-		/* read in neighbor distance */
-		spl = strtok(NULL, " ");
-		n->distance = atof(spl);	
-		neigh_list->push_back(*n);
-	}
-	neigh_list->sort(Neighbor::comp);
-	if (neigh_list->size() > NEAREST) {
-		it = neigh_list->begin();
-		advance(it, NEAREST);	
-		neigh_list->erase(it, neigh_list->end());
-	}
-}
+//void ImagePAO::add(void* val)
+//{	
+//	char* spl;
+//	char* read_buf = (char*)val;
+//	Neighbor* n;
+//	std::list<Neighbor>::iterator it; 
+//	// read key
+//	spl = strtok(read_buf, " ");
+//	while (true) {
+//		/* read in neighbor key */
+//		spl = strtok(NULL, " ");
+//		if (spl == NULL)
+//			break;
+//		/* create new neighbor */
+//		n = new Neighbor(spl, strlen(spl)+1);
+//		/* read in neighbor distance */
+//		spl = strtok(NULL, " ");
+//		n->distance = atof(spl);	
+//		neigh_list->push_back(*n);
+//	}
+//	neigh_list->sort(Neighbor::comp);
+//	if (neigh_list->size() > NEAREST) {
+//		it = neigh_list->begin();
+//		advance(it, NEAREST);	
+//		neigh_list->erase(it, neigh_list->end());
+//	}
+//}
 
 void ImagePAO::merge(PartialAgg* add_agg)
 {
 	ImagePAO* mg_pao = (ImagePAO*)add_agg;
-	std::list<Neighbor>* mg_list = mg_pao->neigh_list;
-	Neighbor* new_neighbor;
-	std::list<Neighbor>::iterator it; 
-	for (it=mg_list->begin(); it != mg_list->end(); ++it) {
-		new_neighbor = new Neighbor(*it);
-		neigh_list->push_back(*new_neighbor);
-	}
-	neigh_list->sort(Neighbor::comp);
-	if (neigh_list->size() > NEAREST) {
-		it = neigh_list->begin();
-		advance(it, NEAREST);	
-		neigh_list->erase(it, neigh_list->end());
-	}
-}
+    mutable_neighbors()->MergeFrom(mg_pao->neighbors());
 
-void ImagePAO::serialize(FILE* f, void* buf, size_t buf_size)
-{
-	serialize(buf);
-	char* wr_buf = (char*)buf;
-	assert(NULL != f);
-	size_t l = strlen(wr_buf);
-	assert(fwrite(wr_buf, sizeof(char), l, f) == l);
-}
+    sort(mutable_neighbors()->begin(),
+         mutable_neighbors()->end(),
+         comparator);
 
-void ImagePAO::serialize(void* buf)
-{
-	char* wr_buf = (char*)buf;
-	char dist[20];
-	std::list<Neighbor>::iterator it; 
-	strcpy(wr_buf, key);
-	for (it = neigh_list->begin(); it != neigh_list->end(); ++it) {
-		strcat(wr_buf, " ");
-		strcat(wr_buf, (*it).key);
-		strcat(wr_buf, " ");
-		sprintf(dist, "%lu", (*it).distance);
-		strcat(wr_buf, dist);
-	}	
-	strcat(wr_buf, "\n");
-}
-
-bool ImagePAO::deserialize(FILE* f, void* buf, size_t buf_size)
-{
-	char *read_buf = (char*)buf;
-	if (feof(f)) {
-		return false;
+	if (neighbors_size() > NEAREST) {
+        mutable_neighbors()->RemoveLast();
 	}
-	if (fgets(read_buf, buf_size, f) == NULL)
-		return false;
-	return deserialize(read_buf);
-}
-
-bool ImagePAO::deserialize(void* buf)
-{
-	char* spl;
-	char* read_buf = (char*)buf;
-	Neighbor* n;
-	neigh_list = new std::list<Neighbor>();
-	spl = strtok(read_buf, " ");
-	if (spl == NULL)
-		return false;
-	key = (char*)malloc(strlen(spl) + 1);
-	strcpy(key, spl);
-	while (true) {
-		/* read in neighbor key */
-		spl = strtok(NULL, " ");
-		if (spl == NULL)
-			break;
-		/* create new neighbor */
-		n = new Neighbor(spl, KEYSIZE);
-		/* read in neighbor distance */
-		spl = strtok(NULL, " ");
-		n->distance = atof(spl);	
-		neigh_list->push_back(*n);
-	}
-	return true;
 }
 
 CImg<float>* ImagePAO::ph_dct_matrix(const int N)
@@ -193,8 +121,7 @@ CImg<float>* ImagePAO::ph_dct_matrix(const int N)
 	return ptr_matrix;
 }
 
-
-void ImagePAO::pHash(CImg<unsigned char> src, unsigned long& hash)
+void ImagePAO::pHash(CImg<unsigned char> src, uint64_t& hash)
 {
 	CImg<float> meanfilter(7,7,1,1,1);
 	CImg<float> img;
@@ -227,6 +154,37 @@ void ImagePAO::pHash(CImg<unsigned char> src, unsigned long& hash)
 			hash |= one;
 		one = one << 1;
 	}
+}
+
+inline void ImagePAO::serialize(google::protobuf::io::CodedOutputStream* output) const
+{
+    output->WriteVarint32(pb.ByteSize());
+    pb.SerializeToCodedStream(output);
+}
+
+inline void ImagePAO::serialize(std::string* output) const
+{
+    pb.SerializeToString(output);
+}
+
+inline bool ImagePAO::deserialize(google::protobuf::io::CodedInputStream* input)
+{
+    uint32_t bytes;
+    input->ReadVarint32(&bytes);
+    google::protobuf::io::CodedInputStream::Limit msgLimit = input->PushLimit(bytes);
+    bool ret = pb.ParseFromCodedStream(input);
+    input->PopLimit(msgLimit);
+    return ret;
+}
+
+inline bool ImagePAO::deserialize(const std::string& input)
+{
+	return pb.ParseFromString(input);
+}
+
+inline bool ImagePAO::deserialize(const char* input, size_t size)
+{
+	return pb.ParseFromArray(input, size);
 }
 
 REGISTER_PAO(ImagePAO);
