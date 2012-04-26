d3f324d5e95a4fdbd30492af18d3f4a153c2e84f
Wolfgang Richter <wolf@cs.cmu.edu>
2012-04-25 22:56:07 -0400

    apps: making img constructor safer, same issue remains?

--- src/lib/apps/img.cpp
+++ src/lib/apps/img.cpp
@@ -19,33 +19,28 @@
  */
 ImagePAO::ImagePAO(Token* token)
 {
-	if (token == NULL) {
-		pb.set_key(NULL);
-        /* nada; leave at size 0 pb.add_neighbors(); */
-		return;
-	}
+	if (token != NULL)
+    {
+        // Set key equal to the query file name
+        pb.set_key((char*) (token->tokens[2]));
 
-	// Set key equal to the query file name
-	pb.set_key((char*) (token->tokens[2]));
+        //	fprintf(stderr, "Key: %s\t", key);
+        // Calculate image hash
+        CImg<unsigned char> img;
+        img.load_jpeg_buffer((JOCTET*)(token->tokens[3]), token->token_sizes[3]);
+        pHash(img, hash);
 
-    //	fprintf(stderr, "Key: %s\t", key);
-	// Calculate image hash
-	CImg<unsigned char> img;
-	img.load_jpeg_buffer((JOCTET*)(token->tokens[3]), token->token_sizes[3]);
-	pHash(img, hash);
+        uint64_t ne_hash;
+        fprintf(stderr, "Neigh: %s\n", (char*)token->tokens[0]);
+        imagepao::Neighbor* n = pb.add_neighbors();
+        n->set_key((char*)(token->tokens[0]));
 
-	uint64_t ne_hash;
-	fprintf(stderr, "Neigh: %s\n", (char*)token->tokens[0]);
-    imagepao::Neighbor* n = pb.add_neighbors();
-	n->set_key((char*)(token->tokens[0]));
-
-	CImg<unsigned char> ne_img;
-	ne_img.load_jpeg_buffer((JOCTET*)(token->tokens[1]),
-			token->token_sizes[1]);
-	pHash(ne_img, ne_hash);
-	n->set_distance(abs((long)(hash - ne_hash)));
-
-//	fprintf(stderr, "Found neighbor: %s, %ld\n", n->key, n->distance);
+        CImg<unsigned char> ne_img;
+        ne_img.load_jpeg_buffer((JOCTET*)(token->tokens[1]),
+                token->token_sizes[1]);
+        pHash(ne_img, ne_hash);
+        n->set_distance(abs((long)(hash - ne_hash)));
+    }
 }
 
 ImagePAO::~ImagePAO()
